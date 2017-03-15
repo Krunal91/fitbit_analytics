2013f0d9f7b2df5b214422537083ba77d757f3e3
Krunal91 <krunalapatel@live.com>
2017-03-14 22:22:33 -0400

    Final working code.

--- server.r
+++ server.r
@@ -1,31 +1,110 @@
 library(shiny)
+library(openssl)
+library(rsconnect)
 library(ggplot2)
 library(fitbitScraper)
-
-function(input, output) {
+library(ggthemes)
+library(plotly)
+library(dplyr)
+library(devtools)
+function(input, output,session) {
   cookie =eventReactive(input$logon,{login(input$email,input$pass)})
   
   options  = reactiveValues(value = 'steps')
   observeEvent(input$steps,{options$value='steps'})
   observeEvent(input$distance,{options$value='distance'})
+  observeEvent(input$floors,{options$value='floors'})
+  observeEvent(input$minutesVery,{options$value='minutesVery'})
+  
+  
+  
+  plt_title = reactiveValues(value = "Number of Steps Per Day")
+  observeEvent(input$steps,{plt_title$value="Number of Steps Per Day"})
+  observeEvent(input$distance,{plt_title$value="Number of Miles Per Day"})
+  observeEvent(input$floors,{plt_title$value="Number of Floors Per Day"})
+  observeEvent(input$minutesVery,{plt_title$value="Number of Active Minutes Per Day"})
+  
+  
+  bmi_range = c(10,16,17,18.5,25,30,35,40,50)
+  body_type = c("Severe Thinness","Moderate Thinness","Mild Thinness",
+                "Normal","Overweight","Obese Class I","Obese Class II",
+                "Obese Class III","Obese Class III")
+  
+  latest_weight = reactive({get_weight_data(cookie(),start_date = as.character(Sys.Date()),end_date = as.character(Sys.Date()))[1,2]})
+  observeEvent(input$logon,{
+    updateNumericInput(session,'weight',value = latest_weight())
+  })
+  
+  bmi_count = eventReactive(input$calculate,{(input$weight/(as.double(input$height)^2))*703})
+  
+  data = reactive({
+    get_daily_data(cookie(),what = options$value,start_date = as.character(input$daterange[1]),
+                   end_date = as.character(input$daterange[2]))})
+  
+  
+  data_activity = reactive({
+    get_activity_data(cookie(),end_date = as.character(input$daterange[2]))})
+  
+  data_activity_2 = reactive({data_activity() %>% select(5,11)})
+  
+  calory_sum = reactive({data_activity_2() %>% group_by(name) %>%summarize(calory =sum(calories)) %>% arrange(desc(calory))})
+  
+  top = reactive({calory_sum()$name[1]})
+  
+  
+  data_calory = reactive({
+    get_daily_data(cookie(),what = "caloriesBurnedVsIntake",start_date = as.character(input$daterange[1]),
+                   end_date = as.character(input$daterange[2]))})
+  data_sleep = reactive({
+    get_sleep_data(cookie(),start_date = as.character(input$daterange[1]),
+                   end_date = as.character(input$daterange[2]))[[2]]})
+  
+  data_sleep_2 = reactive({data_sleep() %>% select(1,5)})
+  month = reactive({months(as.Date(data_sleep_2()$date))})
+  
+  
+  data_heart = reactive({
+    get_daily_data(cookie(),what = "getRestingHeartRateData",start_date = as.character(input$daterange[1]),
+                   end_date = as.character(input$daterange[2]))})
+  
+  theme = theme_gray()+theme(axis.text = element_text(size = 12),
+                             axis.title = element_text(size=16),
+                             plot.title = element_text(size=20,face = 'bold',hjust = 0.5))
+  
+  
+  
+  #######################OUTPUT###################################### 
+  
+  output$bmi =renderText({paste0("Your BMI is  :",bmi_count())})  #eventReactive(input$logon, {print({bmi_count})})
+  
+  
+  output$scatter = renderPlotly({plot_ly(data(),x=data()[,'time'],y=data()[,options$value],mode='lines') %>% layout(title=plt_title$value,
+                                                                                                                    xaxis=list(title='Time'),                                                   yaxis=list(title=options$value))})
+  
+  
+  
+  
+  observeEvent(input$logon,{output$calory_title =renderUI({h5("Total Burned Calories Breakdown By Activity",align="center")
+  })})
+  output$scatter_calory = renderPlotly({plot_ly(calory_sum(),x=~name,y=~calory,type="bar",color = ~name) %>% layout( xaxis=list(title="Activities"),y=list(title="Total Calories Burned"))})
+  #ggplotly()})
+  
+  observeEvent(input$logon,{output$pie_title =renderUI({h5("Percentage BreakDown for Activities",align="center")
+  })})
+  output$pie_act = renderPlotly({plot_ly(data_activity(),labels=~name,values=~calories,type="pie") %>% layout(xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))})
+  
+  observeEvent(input$logon,{output$heart_title =renderUI({h5("Average Heart Rate per day",align="center")
+  })})
+  output$scatter_heart = renderPlotly({plot_ly(data_heart(),x=data_heart()[,'time'],y=data_heart()[,"restingHeartRate"],mode='lines',color = I('purple')) %>% layout(xaxis=list(title='Time'),yaxis=list(title="Resting Heart"))})
+  
+  observeEvent(input$logon,{output$sleep_title =renderUI({h5("Sleep summary for each Month",align="center")})})
+  
+  output$heartVsleep = renderPlotly({plot_ly(data_sleep_2(),y=~sleepDuration,type="box",color=~month()) %>% layout(yaxis=list(title="Sleep Minutes"))})
+  
+  observeEvent(input$calculate,{output$title_6 =renderUI({h5("BMI Comparison Plot",align="center")})})
+  output$bmi_plot = renderPlotly({ plot_ly(x=~seq(9),y=~bmi_range,type = 'scatter',mode='text',text= ~body_type,color = ~body_type) %>% layout(shapes=list(type='line',x0=0,x1=8,y0=bmi_count(),y1=bmi_count(),yref="y",xref="x",line=list(color='blue')),xaxis=list(title=""),yaxis=list(title="BMI Count"))})
   
 
-  data = reactive({
-    get_daily_data(cookie(),what = options$value,start_date = as.character(input$startdate),
-                        end_date = as.character(input$enddate))})
-  
-  
-  
-  output$scatter = renderPlot({ggplot(data(),aes(data()$time,data()[,options$value])) +geom_line()})
-  #output$scatter =renderPlot({hist(rnorm(100))})
-  output$result = renderPrint({options()})
   
 }
 
-
-# options = reactive({switch (input$options,
-#                   Steps = 'steps',
-#                   Distance ='distance',
-#                   Floors='floors',
-#                   Minutes= 'minutesVery'
-# )})
--- ui.r
+++ ui.r
@@ -1,35 +1,112 @@
 library(shiny)
 library(ggplot2)
+library(plotly)
+library(shinythemes)
 
-dataset <- diamonds
 
-fluidPage(
-  
-  titlePanel(h1("FitBit Analytics",align="center")),
-  
-  sidebarPanel(
-    
-    textInput('email', 'Login Email:'),
-    passwordInput('pass', 'Password'),
-    actionButton('logon','LogIn'),
-    dateInput('startdate','Start Date'),
-    dateInput('enddate','End Date'),
-    
-    actionButton('steps','Steps'),
-    actionButton('distance','Distance'),
-    verbatimTextOutput('value')
-  ),
-  
-  mainPanel(
-    plotOutput('scatter'),
-    verbatimTextOutput('result')
-    
-
-  )
+navbarPage("FitBit Analytics",
+           theme = shinytheme("darkly"),
+           
+           tabPanel("Home",
+                    
+                    fluidRow(
+                      column(4,
+                             br(),
+                             textInput('email', 'Login Email:'),
+                             passwordInput('pass', 'Password'),
+                             actionButton("logon","Login"),
+                             #dateInput('startdate','Start Date'),
+                             #dateInput('enddate','End Date'),
+                             tags$br(),tags$br(),
+                             dateRangeInput('daterange',"Select Date Range",start = Sys.Date()-15),
+                             verbatimTextOutput("test")
+                             
+                             
+                             
+                      ),
+                      
+                      column(8,
+                             h5("Choose an Option"),
+                             actionButton('steps','Steps'),
+                             actionButton('distance','Distance'),
+                             actionButton('floors','Floors'),
+                             actionButton('minutesVery','Minutes'),
+                             # actionButton('caloriesBurnedVsIntake','Calories'),
+                             # actionButton('getRestingHeartRateData','Resting HR'),
+                             tags$br(),
+                             tags$br(),
+                             
+                             plotlyOutput('scatter')
+                             
+                      ),
+                      
+                      column(6,
+                             br(),
+                             br(),
+                             uiOutput("calory_title"),
+                             plotlyOutput('scatter_calory')
+                             
+                      ),
+                      
+                      column(6,
+                             br(),
+                             br(),
+                             uiOutput("pie_title"),
+                             plotlyOutput('pie_act')
+                             
+                      ),
+                      
+                      
+                      column(6,
+                             br(),
+                             br(),
+                             uiOutput("heart_title"),
+                             plotlyOutput('scatter_heart')
+                             
+                      ),
+                      column(6,
+                             br(),
+                             br(),
+                             uiOutput("sleep_title"),
+                             plotlyOutput('heartVsleep')
+                             
+                      ))),
+           
+           tabPanel("Health Status",
+                    column(6,
+                           br(),
+                           br(),
+                           h4("Are you healthy or not?"),
+                           br(),
+                           br(),
+                           p("Here, we will count your BMI and then on the plot you will 
+           see, where you stand in that grpah."),
+                           p("Please enter your heigt in inches and weight in lbs,than click on calculate button."),
+                           numericInput('height','Height in Inches',value = 56),
+                           numericInput('weight','Weight in lbs',value = 87),
+                           actionButton("calculate","Calculate"),
+                           br(),
+                           br(),
+                           verbatimTextOutput("bmi")
+                    ),
+                    
+                    column(6,
+                           br(),
+                           br(),
+                           uiOutput("title_6"),
+                           plotlyOutput("bmi_plot")
+                           
+                    )
+           ),
+           
+           
+           
+           tabPanel("About Me",
+                    column(4,
+                           includeHTML("linkedin.html")
+                           )
+                           )
+           
+           
 )
 
-#radioButtons('options','Choose an Option',c('Steps','Distance',
- #                                           'Floors','Minutes')),
-
-# selectInput('facet_row', 'Facet Row', c(None='.', names(dataset))),
-# selectInput('facet_col', 'Facet Column', c(None='.', names(dataset)))
